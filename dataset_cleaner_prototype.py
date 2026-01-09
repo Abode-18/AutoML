284fc63b2b1db9a88869763c69b6dd1b949ea1a4
Abode-18 <abode.bader@outlook.sa>
2026-01-09 16:30:03 +0300

    first upload

---
+++ dataset_cleaner_prototype.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+
+def get_data():
+    path = input("Enter the path to the dataset: ")
+    df = pd.DataFrame(pd.read_csv(path))
+    df['Oldpeak'].value_counts()
+    print("Dataset loaded successfully.")
+    return df
+def get_info_about_df(df):
+    global cat_columns,num_columns
+    cat_columns = df.select_dtypes(include = object).columns.tolist()
+    num_columns = df.select_dtypes(include = int).columns.tolist()
+    for column in df.columns:
+        if column in cat_columns:
+            continue
+        elif column in num_columns:
+            if df[column].value_counts() < 10:
+                del num_columns[num_columns.index(column)]
+    return cat_columns,num_columns
+def get_info_about_column(df,column):
+    info = {}
+    info["missing_ratio"] = df[column].isnull().sum() / len(df[column])
+    if column in cat_columns:
+        info['n_unique'] = df[column].nunique()
+    if column in num_columns:
+        info["mean"] = df[column].mean()
+        info["std"] = df[column].std()
+        info["min"] = df[column].min()
+        info["max"] = df[column].max()
+        info["skewness"] = df[column].skew()
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+        info["outliers_ratio"] = len(outliers) / len(df)
+        info["kurtosis"] = df[column].kurt()
+        info["max_zscore"] = ((df[column] - df[column].mean())/df[column].std()).max()
+def Reg_model_evaluation(model):
+        from sklearn.metrics import r2_score
+        model.fit(X_train,y_train)
+        y_pred = model.predict(X_test)
+        score = r2_score(y_test,y_pred)
+        return score
+def choosing_model():
+
+    models = ["Logistic Regression","Random Forest Regression","Hist Gradient Boosting Regression","Random Forest classifier","Hist Gradient Boosting classifier","autoselect - it will take a while"]
+    # models = {
+    #     "Regression":{
+    #         "Logistic Regression": LogisticRegression(),
+    #         "Random Forest": RandomForestRegressor(),
+    #         "Hist Gradient Boosting": HistGradientBoostingRegressor()
+    #     },
+    #     "Classification":{
+    #         "Random Forest": RandomForestClassifier(),
+    #         "Hist Gradient Boosting": HistGradientBoostingClassifier()
+    #     }
+    #     }
+    print("Choose a model from the following options:")
+    for model in models:
+        print(f"{models.index(model)+1} ",model)
+    choose = int(input("Enter the number corresponding to your choice: "))
+
+    if choose == 1:
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression()
+    elif choose == 2:
+        from sklearn.ensemble import RandomForestRegressor
+        model = RandomForestRegressor
+    elif choose == 3:
+        from sklearn.ensemble import HistGradientBoostingRegressor
+        model = HistGradientBoostingRegressor
+    elif choose == 4:
+        from sklearn.ensemble import RandomForestClassifier
+        model = RandomForestClassifier()
+    elif choose == 5:
+        from sklearn.ensemble import HistGradientBoostingClassifier
+        model = HistGradientBoostingClassifier()
+    else:
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.ensemble import RandomForestRegressor
+        from sklearn.ensemble import HistGradientBoostingRegressor
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.ensemble import HistGradientBoostingClassifier
+        from sklearn.model_selection import train_test_split
+        Reg_models = [LogisticRegression(),RandomForestRegressor(),HistGradientBoostingRegressor()]
+        Clas_models = [RandomForestClassifier(),HistGradientBoostingClassifier()]
+        print("choose the y column of these columns\n")
+        for column in df.columns.tolist():
+            print(column)
+        y_column = input("choose the y column in the dataframe: ")
+        global X_train,X_test,y_test,y_pred,y_train
+        X_train,X_test,y_train,y_test = train_test_split(df.drop(y_column,axis = 1),df[y_column],test_size=0.3,random_state=1)
+        Reg_models[0].fit(X_train,y_train)
+        y_pred = Reg_models[0].predict(X_test)
+        biggest_score = r2_score(y_test,y_pred)
+        for model in Reg_models:
+
+            
+
+
+
+
+def delete_outliers(df):
+    num_col = df.select_dtypes(include=np.number).columns.tolist()
+    for col in num_col:
+        pass
+
+
+
+def main():
+    df=get_data()
+    choosing_model()
+
+main()
